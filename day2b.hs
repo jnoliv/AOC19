841630080f167c1cbe8a884551dcbf79a441d086
João Nuno Oliveira <joaonunoao@gmail.com>
2019-12-22 00:32:03 +0000

    Code improvements on second part of second day

--- day2b.hs
+++ day2b.hs
@@ -1,30 +1,21 @@
-main = do
-    contents <- getContents
-    let codes = map (read :: String -> Int) . split $ contents
-    let res = tryAll codes
-    print (100 * (fst res) + (snd res))
+import Data.List (find)
+import Data.List.Split (splitOn)
 
-split :: String -> [String]
-split l = splitAux ',' l []
+type Program = [Int]
 
-splitAux :: Char -> String -> String -> [String]
-splitAux del [] cur = [cur]
-splitAux del (x:xs) cur =
-    if x == del
-        then cur : splitAux del xs []
-        else splitAux del xs (cur ++ [x])
+toProgram :: String -> Program
+toProgram = map read . splitOn ","
 
 setAt :: Int -> a -> [a] -> [a]
-setAt pos val l = (take pos l) ++ (val : drop (pos + 1) l)
+setAt pos val l = take pos l ++ [val] ++ drop (pos + 1) l
 
 getAt :: Int -> [a] -> a
-getAt 0 l = head l
-getAt n l = head (drop n l)
+getAt pos = head . drop pos
 
-process :: [Int] -> Int
+process :: Program -> Int
 process l = processAux 0 l
 
-processAux :: Int -> [Int] -> Int
+processAux :: Int -> Program -> Int
 processAux ind l =
     let opcode = getAt ind l
         in1 = getAt (getAt (ind + 1) l) l
@@ -36,18 +27,19 @@ processAux ind l =
             2 -> processAux (ind + 4) $ setAt out (in1 * in2) l
             99 -> head l
 
-initMemory :: [Int] -> Int -> Int -> [Int]
-initMemory l n v = (head l) : n : v : (drop 3 l)  
+tryAll :: Program -> Int -> (Int, Int)
+tryAll prog val =
+    dropLast $ find (\(_,_,v) -> val == v) [(n, v, process' n v) | n <- [0..99], v <- [0..99]]
+    where
+        process' n v = process . setAt 1 n . setAt 2 v $ prog
+        dropLast (Just (n,v,_)) = (n,v)
+        dropLast Nothing = (-1, -1)
 
-tryAll :: [Int] -> (Int, Int)
-tryAll l = tryAllAux l 0 0
+main:: IO ()
+main = do
+    contents <- getContents
 
-tryAllAux :: [Int] -> Int -> Int -> (Int, Int)
-tryAllAux l 100 n = (-1, -1) 
-tryAllAux l n 100 = tryAllAux l (n + 1) 0
-tryAllAux l i j =
-    let memory = initMemory l i j
-        res = process memory
-    in if res == 19690720
-        then (i, j)
-        else tryAllAux l i (j + 1)
+    let codes = toProgram contents
+    let (noun, verb) = tryAll codes 19690720
+
+    print $ 100 * noun + verb
