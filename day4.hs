f4c65151fe9b766ed67d5fe617e64f1848a34254
João Nuno Oliveira <joaonunoao@gmail.com>
2019-12-23 23:16:05 +0000

    Merge two parts of day four

--- day4.hs
+++ day4.hs
@@ -1,25 +1,19 @@
 import Data.Char (digitToInt)
+import Data.List (sort, group)
 import Data.List.Split (splitOn)
 
-main = do
-    contents <- getContents
+countPossiblePasses :: ([Int] -> Bool) -> [[Int]] -> Int
+countPossiblePasses f [b,e] = countPossiblePasses' f b e 0 []
 
-    let range = map (map digitToInt) . splitOn "-" $ contents
+countPossiblePasses' :: ([Int] -> Bool) -> [Int] -> [Int] -> Int -> [Int] -> Int
+countPossiblePasses' f beg end 6 cur = 
+    if f cur then 1 else 0
 
-    print $ countPossiblePasses range
-
-countPossiblePasses :: [[Int]] -> Int
-countPossiblePasses [b,e] = countPossiblePasses' b e 0 []
-
-countPossiblePasses' :: [Int] -> [Int] -> Int -> [Int] -> Int
-countPossiblePasses' beg end 6 cur = 
-    if hasTwoAdjacentEqual cur then 1 else 0
-
-countPossiblePasses' beg end ind cur = 
+countPossiblePasses' f beg end ind cur = 
     if isNotInRange beg end cur
         then 0
         else
-            let applied = countPossiblePasses' beg end (ind + 1)
+            let applied = countPossiblePasses' f beg end (ind + 1)
                 lastChar = if length cur == 0 then 0 else (last cur)
             in sum . map applied $ [cur ++ [c] | c <- [lastChar..9]]
 
@@ -42,3 +36,19 @@ hasTwoAdjacentEqual :: [Int] -> Bool
 hasTwoAdjacentEqual (x:[]) = False
 hasTwoAdjacentEqual (x:xs) = 
     if x == (head xs) then True else hasTwoAdjacentEqual xs
+
+hasExactlyTwoAdjacentEqual :: [Int] -> Bool
+hasExactlyTwoAdjacentEqual l =
+    any (== 2) $ map length . group . sort $ l
+
+main :: IO ()
+main = do
+    contents <- getContents
+
+    let range = map (map digitToInt) . splitOn "-" $ contents
+
+    let output1 = countPossiblePasses hasTwoAdjacentEqual range
+    let output2 = countPossiblePasses hasExactlyTwoAdjacentEqual range
+
+    putStrLn $ "Part1: " ++ show output1
+    putStrLn $ "Part2: " ++ show output2
