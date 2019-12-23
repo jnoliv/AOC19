29a6519666305dc874212a06de0c6364fb7666dc
João Nuno Oliveira <joaonunoao@gmail.com>
2019-12-23 18:49:50 +0000

    Code improvements on first part of fourth day

--- day4.hs
+++ day4.hs
@@ -1,64 +1,44 @@
 import Data.Char
+import Data.List.Split
 
 main = do
     contents <- getContents
-    let range = map (map digitToInt) . split $ contents
-    print (countPossiblePasses range)
 
-split :: String -> [String]
-split l = splitAux '-' l []
+    let range = map (map digitToInt) . splitOn "-" $ contents
 
-splitAux :: Char -> String -> String -> [String]
-splitAux del [] cur = [cur]
-splitAux del (x:xs) cur =
-    if x == del
-        then cur : splitAux del xs []
-        else splitAux del xs (cur ++ [x])
+    print $ countPossiblePasses range
 
 countPossiblePasses :: [[Int]] -> Int
-countPossiblePasses (b:e) =
-    countPossiblePassesAux b (head e) 0 0 []
+countPossiblePasses [b,e] = countPossiblePasses' b e 0 []
 
-countPossiblePassesAux :: [Int] -> [Int] -> Int -> Int -> [Int] -> Int
-countPossiblePassesAux beg end 6 count cur = 
-    if (hasTwoAdjacentEqual cur) then 1 else 0
+countPossiblePasses' :: [Int] -> [Int] -> Int -> [Int] -> Int
+countPossiblePasses' beg end 6 cur = 
+    if hasTwoAdjacentEqual cur then 1 else 0
 
-countPossiblePassesAux beg end ind count cur = 
-    if not (isInRange beg end cur) then 0
-        else let applied = countPossiblePassesAux beg end (ind + 1) count
-        in sumPossibleChildPasses applied cur (
-            if (length cur) == 0 then 0 else (last cur))
+countPossiblePasses' beg end ind cur = 
+    if isNotInRange beg end cur
+        then 0
+        else
+            let applied = countPossiblePasses' beg end (ind + 1)
+                lastChar = if length cur == 0 then 0 else (last cur)
+            in sum . map applied $ [cur ++ [c] | c <- [lastChar..9]]
 
-sumPossibleChildPasses :: ([Int] -> Int) -> [Int] -> Int -> Int
-sumPossibleChildPasses f cur 10 = 0
-sumPossibleChildPasses f cur ind =
-    (f (cur ++ [ind])) + (sumPossibleChildPasses f cur (ind + 1))
-
-
-isInRange :: [Int] -> [Int] -> [Int] -> Bool
-isInRange b e c = (c `isGreaterOrEqual` b) && (c `isLessOrEqual` e)
+isNotInRange :: [Int] -> [Int] -> [Int] -> Bool
+isNotInRange b e c = not $ (c `isGreaterOrEqual` b) && (c `isLessOrEqual` e)
 
 isGreaterOrEqual :: [Int] -> [Int] -> Bool
 isGreaterOrEqual [] y = True
 isGreaterOrEqual (x:xs) (y:ys) =
-    if x < y
-        then False
-        else if x > y
-            then True
-            else isGreaterOrEqual xs ys
+    if x < y then False else
+        if x > y then True else isGreaterOrEqual xs ys
 
 isLessOrEqual :: [Int] -> [Int] -> Bool
 isLessOrEqual [] y = True
 isLessOrEqual (x:xs) (y:ys) =
-    if x > y
-        then False
-        else if x < y
-            then True
-            else isLessOrEqual xs ys
+    if x > y then False else
+        if x < y then True else isLessOrEqual xs ys
 
 hasTwoAdjacentEqual :: [Int] -> Bool
 hasTwoAdjacentEqual (x:[]) = False
 hasTwoAdjacentEqual (x:xs) = 
-    if x == (head xs)
-        then True
-        else hasTwoAdjacentEqual xs
+    if x == (head xs) then True else hasTwoAdjacentEqual xs
