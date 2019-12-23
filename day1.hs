b5e9178825df0f4aee739f936f5f0c210d4c1cd5
João Nuno Oliveira <joaonunoao@gmail.com>
2019-12-23 22:58:48 +0000

    Merge two parts of day one

--- day1.hs
+++ day1.hs
@@ -1,10 +1,16 @@
 calcFuel :: Integral a => a -> a
 calcFuel = flip (-) 2 . flip div 3
 
+calcWithFuel :: Integral a => a -> a
+calcWithFuel = sum . takeWhile (> 0) . tail . iterate calcFuel
+
 main :: IO ()
 main = do
     contents <- getContents
     let masses = map read . lines $ contents
 
     let totalFuel = sum . map calcFuel $ masses
-    print totalFuel
+    let totalFuelWithFuel = sum . map calcWithFuel $ masses
+
+    putStrLn $ "Part1: " ++ show totalFuel
+    putStrLn $ "Part2: " ++ show totalFuelWithFuel
