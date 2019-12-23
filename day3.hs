bd80edf2f9251633743c954bab4aaf29f057daa7
João Nuno Oliveira <joaonunoao@gmail.com>
2019-12-23 12:09:43 +0000

    Code improvements on first part of third day

--- day3.hs
+++ day3.hs
@@ -1,56 +1,45 @@
-import qualified Data.Set as S
+import Data.List.Split (splitOn)
+import qualified Data.Set as Set
 
-main = do
-    contents <- getContents
-    let ls = lines contents
-    let l1 = map toDir . split $ (head ls)
-    let l2 = map toDir . split $ (head (drop 1 ls))
-    let ints = S.toList $ S.intersection (pointsSet l1) (pointsSet l2)
-    let dists = map distance ints
-    print (minimum dists)
+type Point = (Int, Int)
+type PointSet = Set.Set Point
 
+add :: Point -> Point -> Point
+(x1,y1) `add` (x2,y2) = (x1 + x2, y1 + y2)
 
-split :: String -> [String]
-split l = splitAux ',' l []
+mult :: Int -> Point -> Point
+n `mult` (x,y) = (n * x, n * y)
 
-splitAux :: Char -> String -> String -> [String]
-splitAux del [] cur = [cur]
-splitAux del (x:xs) cur =
-    if x == del
-        then cur : splitAux del xs []
-        else splitAux del xs (cur ++ [x])
+dist :: Point -> Point -> Int
+dist (x1,y1) (x2,y2) = (abs $ x1 - x2) + (abs $ y1 - y2)
+
 
 toDir :: String -> (Char, Int)
 toDir (dir:count) = (dir, read count)
 
-pointsSet :: [(Char, Int)] -> S.Set (Int, Int)
-pointsSet points = pointsSetAux (0,0) points S.empty
+pointsSet :: [(Char, Int)] -> PointSet
+pointsSet points = pointsSet' (0,0) points
 
-
-pointsSetAux :: (Int, Int) -> [(Char, Int)] -> S.Set (Int, Int) -> S.Set (Int, Int)
-pointsSetAux curPoint [] res = res
-pointsSetAux curPoint (point:points) res = 
-    let len = snd point
-        dir = case fst point of
+pointsSet' :: Point -> [(Char, Int)] -> PointSet
+pointsSet' curPoint [] = Set.empty
+pointsSet' curPoint (dir:dirs) = 
+    let len = snd dir
+        pDir = case fst dir of
             'U' -> (0, 1)
             'R' -> (1, 0)
             'D' -> (0, -1)
             'L' -> (-1, 0)
-        nextPoint = addToPoint curPoint $ multPoint dir len
-        nextSet = addPoints curPoint dir len res
-    in pointsSetAux nextPoint points nextSet
+        nextPoint = curPoint `add` (len `mult` pDir)
+        newPoints = Set.fromList $ map (add curPoint) [n `mult` pDir | n <- [0..(len - 1)]]
+    in Set.union newPoints $ pointsSet' nextPoint dirs
 
-multPoint :: (Int, Int) -> Int -> (Int, Int)
-multPoint (x, y) n = (x * n, y * n)
+main :: IO ()
+main = do
+    contents <- getContents
 
-addPoints :: (Int, Int) -> (Int, Int) -> Int -> S.Set (Int, Int) -> S.Set (Int, Int)
-addPoints curPoint dir 0 set = set
-addPoints curPoint dir count set = 
-    let newPoint = addToPoint curPoint (multPoint dir count)
-    in addPoints curPoint dir (count - 1) (S.insert newPoint set)
+    let [l1,l2] = map (map toDir . splitOn ",") $ lines contents
 
-addToPoint :: (Int, Int) -> (Int, Int) -> (Int, Int)
-addToPoint (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+    let ints = Set.toList . Set.delete (0,0) $ Set.intersection (pointsSet l1) (pointsSet l2)
+    let minDist = minimum $ map (dist (0,0)) ints
 
-distance :: (Int, Int) -> Int
-distance (x, y) = (abs x) + (abs y)
+    print minDist
