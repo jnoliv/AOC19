f212cb7987f7fc49f13f0014d9f9e10bd7f74040
João Nuno Oliveira <joaonunoao@gmail.com>
2019-12-23 22:54:58 +0000

    Merge two parts and code improvements on sixth day

--- day6.hs
+++ day6.hs
@@ -1,21 +1,47 @@
-import Data.List.Split
+import Data.List.Split (splitOn)
+import Data.Maybe (mapMaybe)
 
 data Tree = Node String [Tree]
 
-main = do
-    contents <- getContents
-    let objects = map (splitOn ")") . lines $ contents
-    print . treeChecksum 0 $ makeTree objects "COM"
+makeTree :: [[String]] -> String -> Tree 
+makeTree list root = Node root (map (makeTree list) $ getChildren list root)
+
+getChildren :: [[String]] -> String -> [String]
+getChildren list parent =
+    let append [p,c] r = if (parent == p) then (c:r) else r
+    in foldr append [] list
 
 treeChecksum :: Int -> Tree -> Int
 treeChecksum depth (Node _ []) = depth
 treeChecksum depth (Node _ children) =
     (+) depth $ sum . map (treeChecksum (depth + 1)) $ children
 
-makeTree :: [[String]] -> String -> Tree 
-makeTree list root = Node root (map (makeTree list) $ getChildren list root) 
+buildPathToLeaf :: String -> Tree -> Maybe [String]
+buildPathToLeaf leafName (Node name []) =
+    if name == leafName then Just [name] else Nothing
 
-getChildren :: [[String]] -> String -> [String]
-getChildren list parent =
-    let append (p:c:[]) r = if (parent == p) then (c:r) else r
-    in foldr append [] list
+buildPathToLeaf nodeName (Node name children) =
+    case mapMaybe (buildPathToLeaf nodeName) children of
+        []   -> Nothing
+        [list] -> Just (name:list)
+
+calcDist :: [String] -> [String] -> Int
+calcDist (x:xs) (y:ys) =
+    if x == y
+        then calcDist xs ys
+        else (length xs) + (length ys)
+
+main :: IO ()
+main = do
+    contents <- getContents
+
+    let objects = map (splitOn ")") . lines $ contents
+    let tree = makeTree objects "COM"
+    let treeCheck = treeChecksum 0 $ makeTree objects "COM"
+
+    let Just pathYOU = buildPathToLeaf "YOU" tree
+    let Just pathSAN = buildPathToLeaf "SAN" tree
+    let dist = calcDist pathYOU pathSAN
+
+    putStrLn $ "Part1: " ++ show treeCheck
+    putStrLn $ "Part2: " ++ show dist 
