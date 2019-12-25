e65c0d9a98a5d62ea23a2e31217dcd7b0c897f5a
João Nuno Oliveira <joaonunoao@gmail.com>
2019-12-25 17:30:57 +0000

    Fix intcode to allow lazy IO

--- Intcode.hs
+++ Intcode.hs
@@ -25,10 +25,10 @@ getAt pos l = case Map.lookup pos l of
     Nothing -> 0
 
 process :: Program -> [Integer] -> [Integer]
-process l input = processAux 0 0 input [] l
+process l input = processAux 0 0 input l
 
-processAux :: Int -> Int -> [Integer] -> [Integer] -> Program -> [Integer]
-processAux ind relB inL outL prog =
+processAux :: Int -> Int -> [Integer] -> Program -> [Integer]
+processAux ind relB inL prog =
     let paramsOpcode = fromInteger $ getAt ind prog
         opcode = paramsOpcode `rem` 100
         pMode1 = (paramsOpcode `div` 100) `rem` 10
@@ -36,44 +36,43 @@ processAux ind relB inL outL prog =
         pMode3 = (paramsOpcode `div` 10000) `rem` 10
     in case opcode of
         -- add first two params and store in third
-        1 -> doAlgOp (+) ind relB inL outL prog pMode1 pMode2 pMode3
+        1 -> doAlgOp (+) ind relB inL prog pMode1 pMode2 pMode3
         -- multiply first two params and store in third
-        2 -> doAlgOp (*) ind relB inL outL prog pMode1 pMode2 pMode3
+        2 -> doAlgOp (*) ind relB inL prog pMode1 pMode2 pMode3
         -- read one int from input and store in first param
         3 -> let param1 = getOutputParam prog (ind + 1) relB pMode1
                  value = head inL
-            in processAux (ind + 2) relB (tail inL) outL $ setAt param1 value prog 
+            in processAux (ind + 2) relB (tail inL) $ setAt param1 value prog 
         -- read from first param and output it
         4 -> let param1 = getParam prog (ind + 1) relB pMode1
-                 newOutL = outL ++ [param1]
-            in processAux (ind + 2) relB inL newOutL prog
+             in param1 : processAux (ind + 2) relB inL prog
         -- if first param != 0, set ind to second param
-        5 -> doJmpOp ((/=) 0) ind relB inL outL prog pMode1 pMode2
+        5 -> doJmpOp ((/=) 0) ind relB inL prog pMode1 pMode2
         -- if first param == 0, set ind to second param
-        6 -> doJmpOp ((==) 0) ind relB inL outL prog pMode1 pMode2
+        6 -> doJmpOp ((==) 0) ind relB inL prog pMode1 pMode2
         -- if first param < second param, store 1 in third param, else store 0
-        7 -> doCmpOp (<) ind relB inL outL prog pMode1 pMode2 pMode3
+        7 -> doCmpOp (<) ind relB inL prog pMode1 pMode2 pMode3
         -- if first param == second param, store 1 in third param, else store 0
-        8 -> doCmpOp (==) ind relB inL outL prog pMode1 pMode2 pMode3
+        8 -> doCmpOp (==) ind relB inL prog pMode1 pMode2 pMode3
         -- adjust relative base
         9 -> let param1 = fromInteger $ getParam prog (ind + 1) relB pMode1 
-             in processAux (ind + 2) (relB + param1) inL outL prog
+             in processAux (ind + 2) (relB + param1) inL prog
         -- halt execution
-        99 -> outL
+        99 -> []
     where
-        doAlgOp algOp ind relB inL outL prog pMode1 pMode2 pMode3 =
+        doAlgOp algOp ind relB inL prog pMode1 pMode2 pMode3 =
             let (param1, param2) = getParams prog ind relB pMode1 pMode2
                 param3 = getOutputParam prog (ind + 3) relB pMode3
-            in processAux (ind + 4) relB inL outL $ setAt param3 (algOp param1 param2) prog
-        doJmpOp jmpOp ind relB inL outL prog pMode1 pMode2 =
+            in processAux (ind + 4) relB inL $ setAt param3 (algOp param1 param2) prog
+        doJmpOp jmpOp ind relB inL prog pMode1 pMode2 =
             let (param1, param2) = getParams prog ind relB pMode1 pMode2
                 nextInd = if jmpOp param1 then (fromInteger param2) else (ind + 3)
-            in processAux nextInd relB inL outL prog
-        doCmpOp cmpOp ind relB inL outL prog pMode1 pMode2 pMode3 =
+            in processAux nextInd relB inL prog
+        doCmpOp cmpOp ind relB inL prog pMode1 pMode2 pMode3 =
             let (param1, param2) = getParams prog ind relB pMode1 pMode2
                 param3 = getOutputParam prog (ind + 3) relB pMode3
                 value = if (param1 `cmpOp` param2) then 1 else 0
-            in processAux (ind + 4) relB inL outL $ setAt param3 value prog
+            in processAux (ind + 4) relB inL $ setAt param3 value prog
 
 getParams :: Program -> Int -> Int -> Int -> Int -> (Integer, Integer)
 getParams prog ind relB pMode1 pMode2 =
