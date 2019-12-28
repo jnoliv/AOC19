7458f59bda992eaa39d359189384c01e1e789f00
João Nuno Oliveira <joaonunoao@gmail.com>
2019-12-28 11:46:57 +0000

    Code improvements on eighth day
    Also merged the solutions of both parts.

--- day8.hs
+++ day8.hs
@@ -1,24 +1,51 @@
-import Data.Char
-import Data.List
-import Data.List.Split
-import Data.Maybe
+import Data.Char (digitToInt)
+import Data.List (elemIndex)
+import Data.List.Split (chunksOf)
+import Data.Maybe (fromJust)
+import System.Environment (getArgs)
+
+toLayers :: Int -> Int -> String -> [[Int]]
+toLayers nCols nRows =
+    chunksOf (nCols * nRows) . map digitToInt . init
+
+checkImage :: [[Int]] -> Int
+checkImage layers =
+    let count0s = map (countIf (== 0)) layers
+        layerMin0s = layers !! (indexOfMinimum count0s)
+        count1s = countIf (== 1) layerMin0s
+        count2s = countIf (== 2) layerMin0s
+    in count1s * count2s
+    where
+        countIf pred = length . filter pred
+        indexOfMinimum l = fromJust $ elemIndex (minimum l) l
+
+toImage :: Int -> [[Int]] -> [[Int]]
+toImage nCols = chunksOf nCols . colapseLayers
+
+colapseLayers :: [[Int]] -> [Int]
+colapseLayers [] = []
+colapseLayers layers =
+    let remaining = length . head $ layers
+    in if remaining == 0
+        then []
+        else (colapsePixel $ map head layers) : (colapseLayers $ map tail layers)
+    where
+        colapsePixel = foldr1 (\f b -> if (f == 2) then b else f)
+
+toString :: [[Int]] -> String
+toString = unlines . map (map (\c -> if (c /= 1) then ' ' else '#'))
 
 main :: IO ()
 main = do
+    args <- getArgs
+    let (nCols,nRows) = if length args /= 2
+                            then (25,6)
+                            else (read $ args !! 0, read $ args !! 1)
+
     contents <- getContents
-    let digits = map digitToInt $ take ((length contents) - 1) contents
-    let layers = chunksOf (25 * 6) digits
+    let layers = toLayers nCols nRows contents
 
-    let count0s = map (countIf (0 ==)) layers
-    let layerMin0s = layers !! (indexOfMinimum count0s)
+    putStrLn $ "Part1: " ++ show (checkImage layers)
 
-    let count1s = countIf (1==) layerMin0s
-    let count2s = countIf (2==) layerMin0s
-    print $ count1s * count2s
-
-
-countIf :: (a -> Bool) -> [a] -> Int
-countIf pred l = length . filter pred $ l
-
-indexOfMinimum :: Ord a => [a] -> Int
-indexOfMinimum l = fromMaybe (-1) $ elemIndex (minimum l) l
+    let image = toImage nCols layers
+    putStrLn $ "Part2: \n" ++ toString image
